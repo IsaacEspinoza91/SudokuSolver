c28ad76fd6491a0903779c0cec750036bde7ef82
Isaac Espinoza <isaac.espinoza.b@usach.cl>
2024-09-01 10:26:59 -0400

    documentacion

--- sudoku.pl
+++ sudoku.pl
@@ -25,17 +25,24 @@ sudoku([
 
 
 
-%suma 45 que locura
+/* Verificar que todo una fila (horizontal) tenga numeros sin repetir
+DOM: lista (LIST)
+REC: bool */
 horizontalCompleto(LIST):-
     verificarUnicoNum(LIST,1),verificarUnicoNum(LIST,2),verificarUnicoNum(LIST,3),
     verificarUnicoNum(LIST,4),verificarUnicoNum(LIST,5),verificarUnicoNum(LIST,6),
     verificarUnicoNum(LIST,7),verificarUnicoNum(LIST,8),verificarUnicoNum(LIST,9).
 
+/* Verificar que todas las fila (horizontal) de una estructura Sudoku tenga numeros sin repetir
+DOM: estructura Sudoku (Lista X Lista)
+REC: bool */
 todoHorizontalCompleto([]).
 todoHorizontalCompleto([CAB_Sudoku|COLA]):-horizontalCompleto(CAB_Sudoku),todoHorizontalCompleto(COLA).
 
 
-
+/* Verificacion de que una lista tenga una sola vez un numero N ingresado
+DOM: lista (LIST) X n (num)
+REC: bool */
 verificarUnicoNum(Lista,N):-verificarUnicoNum(Lista,N,false).
 verificarUnicoNum([],_,true).
 verificarUnicoNum([X|COLA],N,Existe):- X==N, Existe==false ,verificarUnicoNum(COLA,N,true),!.
@@ -45,9 +52,17 @@ verificarUnicoNum([X|COLA],N,Existe):- X\=N, verificarUnicoNum(COLA,N,Existe).
 
 
 
-verticalCompleto(C1,C2,C3,C4,C5,C6,C7,C8,C9):-horizontalCompleto([C1,C2,C3,C4,C5,C6,C7,C8,C9]).
-
+/* Verificacion de que todas las columnas (verticales) de una estructura Sudoku tengan numeros sin repetidos
+DOM: estructura Sudoku (Lista X Lista)
+REC: bool */
 todoVerticalCompleto([[],[],[],[],[],[],[],[],[]]).
 todoVerticalCompleto([[C1|COLA1],[C2|COLA2],[C3|COLA3],[C4|COLA4],[C5|COLA5],[C6|COLA6],[C7|COLA7],[C8|COLA8],[C9|COLA9]]):-
     horizontalCompleto([C1,C2,C3,C4,C5,C6,C7,C8,C9]),
     todoVerticalCompleto([COLA1,COLA2,COLA3,COLA4,COLA5,COLA6,COLA7,COLA8,COLA9]).
+
+
+
+/* Verificacion sudoku correctamente resuelto
+DOM: estructura Sudoku (Lista X Lista)
+REC: bool */
+sudokuResuelto(SUDO):-todoHorizontalCompleto(SUDO),todoVerticalCompleto(SUDO).
